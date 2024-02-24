bfbb628338717550e37ba35dfa242884e6fb28bd
Junxiao <W0486229@campus.nscc.ca>
2024-02-23 21:38:05 -0400

    Updated Front Page

---
+++ MyMovies/Pages/DisplayMovie.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyMovies.Pages
+{
+    public class DisplayMovieModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MyMovies/Pages/Index.cshtml.cs
+++ MyMovies/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyMovies.Data;
+using MyMovies.Models;
 
 namespace MyMovies.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Data.MyMoviesContext _context;
+        public IList<Movie> Movies { get; set; } = default!;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, MyMoviesContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
 
+            Movies = await _context.Movie.ToListAsync();
         }
     }
 }
