68bf0ca81059f45054b2d9457cffb8e9d5ea641f
Junxiao <W0486229@campus.nscc.ca>
2024-02-23 22:32:18 -0400

    Updated UI in DisplayMovie page

--- MyMovies/Pages/DisplayMovie.cshtml.cs
+++ MyMovies/Pages/DisplayMovie.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyMovies.Data;
+using MyMovies.Models;
 
 namespace MyMovies.Pages
 {
     public class DisplayMovieModel : PageModel
     {
-        public void OnGet()
+        private readonly MyMovies.Data.MyMoviesContext _context;
+
+        public Movie Movie { get; set; } = default!;
+
+        // Constructor
+        public DisplayMovieModel(MyMoviesContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.MovieId == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Movie = movie;
+            }
+            return Page();
         }
     }
 }
